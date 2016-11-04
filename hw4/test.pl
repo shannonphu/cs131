1918c0011e622e37f07f51abdba4294d12558efe
Shannon Phu <shannonphu@sbcglobal.net>
2016-11-03 22:25:44 -0700

    deal with ambiguity

--- hw4/test.pl
+++ hw4/test.pl
@@ -1,24 +1,29 @@
-% list helper rules
-len([ ], 0).
-len([H|T], N) :- len(T, N1), N is N1 + 1.
-
     dih(Bit,Count) :- Bit==1, Count==1.
     dih(Bit,Count) :- Bit==1, Count==2.
+    dah(Bit,Count) :- Bit==1, Count>3.    
     dah(Bit,Count) :- Bit==1, Count==3.
+    dah(Bit, Count) :- Bit==1, Count==2.
+    
     delim(Bit,Count) :- Bit==0, Count==1.
+    delim(Bit, Count) :- Bit==0, Count==2.
     boundry(Bit,Count) :- Bit==0, Count==3.
+    boundry(Bit,Count) :- Bit==0, Count==4.
+    boundry(Bit,Count) :- Bit==0, Count==5.
+    boundry(Bit,Count) :- Bit==0,Count==2.
+    space(Bit,Count) :- Bit==0, Count>5.    
     space(Bit,Count) :- Bit==0, Count==7.
+    space(Bit,Count) :- Bit==0, Count==5.
     
     signal_to_morse((B,N), .) :- dih(B,N), !.
     signal_to_morse((B,N), -) :- dah(B,N), !.
-    signal_to_morse((B,N), #) :- space(B,N), !.
-    signal_to_morse((B,N), ^) :- boundry(B,N), !.
     signal_to_morse((B,N), delimiter) :- delim(B,N), !.
+    signal_to_morse((B,N), ^) :- boundry(B,N), !.
+    signal_to_morse((B,N), #) :- space(B,N), !.
 
     compress(L, C) :- pre_compress(L, 1, C).
     pre_compress([], _, []).
     pre_compress([H], Count, [Sym]) :- signal_to_morse((H,Count), Sym), !.
-    pre_compress([H,H|T], Count, TC) :- Cnt2 is Count + 1, pre_compress([H|T], Cnt2, TC), !.
+    pre_compress([H,H|T], Count, TC) :- Cnt2 is Count + 1, pre_compress([H|T], Cnt2, TC). %, !.
     % compresses to [[0,3],[1,3]]
     % pre_compress([H1,H2|T], Count, [[H1,Count]|TC]) :- pre_compress([H2|T], 1, TC).
     % compresses and translates to morse
@@ -28,4 +33,4 @@ len([H|T], N) :- len(T, N1), N is N1 + 1.
     remove_delim([delimiter|T], L) :- remove_delim(T, L), !.
     remove_delim([H|T], [H|T2]) :- remove_delim(T, T2).
 
-    morse(Bits, Res) :- compress(Bits, Temp), remove_delim(Temp, Res).
+    signal_morse(Bits, Res) :- compress(Bits, Temp), remove_delim(Temp, Res).
